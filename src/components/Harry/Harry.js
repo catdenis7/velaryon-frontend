6ee66d4341cbee78bd9d3732dad304723eda6946
catdenis7 <catdenisgomez7@gmail.com>
2022-10-21 13:43:51 -0400

    Primer Commit

---
+++ src/components/Harry/Harry.js
@@ -0,0 +1,11 @@
+import harryPic from '../../assets/images/harry.jpg';
+// EJEMPLO DE COMPONENTE
+export default function Patito () {
+  return (
+    <>
+      <div className="my-3 text-center">
+        <img src={harryPic} alt="Patito Pic" className="img-fluid" width="100"/>
+      </div>
+    </>
+  )
+}
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,27 @@
+import logo from '../../logo.svg';
+import '../../assets/css/style.css';
+import Harry from '../../components/Harry/Harry';
+
+export default function Home() {
+    return (
+        <div className="Home">
+          <header className="Home-header">
+            <img src={logo} className="Home-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <Harry></Harry>
+            <button type="button" className="btn btn-primary">Primary</button>
+            <a
+              className="Home-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      );
+}
+  
